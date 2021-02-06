dfb9fd7e32d5cf4f1ea1be30ac3d010a9b0a821e
Tom Broad <tom+github@uberhq.co.uk>
2021-02-06 19:36:12 +0000

    [app] Initial working version completed

---
+++ src/Midiot/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿namespace Midiot
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Windows;
+    using Microsoft.Win32;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private MidiHandler handler;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            this.StartButton.Click += this.StartButtonOnClick;
+            this.StopButton.Click += this.StopButtonOnClick;
+            this.LoadButton.Click += this.LoadButtonOnClick;
+
+            this.StopButton.IsEnabled = false;
+            this.StartButton.IsEnabled = false;
+
+            var inputDevices = MidiHandler.GetInputDevices().ToList();
+            var outputDevices = MidiHandler.GetOutputDevices().ToList();
+
+            foreach (var device in inputDevices)
+            {
+                this.MidiInput.Items.Add(device);
+            }
+
+            foreach (var device in outputDevices)
+            {
+                this.MidiOutput.Items.Add(device);
+            }
+
+            this.MidiInput.SelectedItem = inputDevices.First(x => !x.Contains("microsoft", StringComparison.CurrentCultureIgnoreCase));
+            this.MidiOutput.SelectedItem = outputDevices.First(x => !x.Contains("microsoft", StringComparison.CurrentCultureIgnoreCase));
+        }
+
+        private async void LoadButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            this.handler?.Stop();
+            this.handler?.Dispose();
+
+            this.handler = new MidiHandler(this.MidiInput.SelectedIndex, this.MidiOutput.SelectedIndex)
+            {
+                SendInitialMessage = true
+            };
+
+            var dialogue = new OpenFileDialog
+            {
+                AddExtension = true,
+                CheckFileExists = true
+            };
+
+            if (dialogue.ShowDialog() ?? false)
+            {
+                await this.handler.LoadAsync(dialogue.FileName);
+
+                this.CommandList.Items.Clear();
+                this.CurrentCommand.Text = string.Empty;
+
+                foreach (var command in FormatCommandList(this.handler.GetCommandList()))
+                {
+                    this.CommandList.Items.Add(command);
+                }
+
+                this.StartButton.IsEnabled = true;
+            }
+        }
+
+        private static IEnumerable<string> FormatCommandList(IEnumerable<string> commandList)
+        {
+            var commands = commandList.ToList();
+            var widest = commands.Max(x => x.LastIndexOf("]", StringComparison.OrdinalIgnoreCase));
+
+            foreach (var command in commands)
+            {
+                var commandParts = command.Split("]");
+                var padding = string.Concat(Enumerable.Repeat(" ", widest - commandParts[0].Length + 1));
+
+                yield return $"{commandParts[0].ToUpperInvariant()}]{padding}{commandParts[1]}";
+            }
+        }
+
+        private void StopButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            this.handler.Stop();
+
+            this.LoadButton.IsEnabled = true;
+            this.StartButton.IsEnabled = true;
+            this.StopButton.IsEnabled = false;
+
+            this.handler.SendInitialMessage = false;
+
+            this.MidiInput.IsEnabled = true;
+            this.MidiOutput.IsEnabled = true;
+        }
+
+        private void StartButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            this.handler.SendInitialMessage = this.SendInitial.IsChecked ?? true;
+            this.handler.Input += this.HandlerOnInput;
+            this.handler.Start();
+
+            this.StopButton.IsEnabled = true;
+            this.LoadButton.IsEnabled = false;
+            this.StartButton.IsEnabled = false;
+
+            this.MidiInput.IsEnabled = false;
+            this.MidiOutput.IsEnabled = false;
+        }
+
+        private void HandlerOnInput(object sender, EventArgs e)
+        {
+            var command = this.handler.GetCurrentCommand();
+            var index = this.handler.GetCurrentIndex();
+
+            this.Dispatcher.Invoke(() =>
+            {
+                this.CommandList.SelectedIndex = index;
+                this.CurrentCommand.Text = FormatCommand(command);
+            });
+        }
+
+        private static string FormatCommand(string command)
+        {
+            var commandParts = command.Split("]");
+
+            return commandParts[0].ToUpper() + "]\t" + commandParts[1].Trim();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            this.handler?.Dispose();
+
+            base.OnClosing(e);
+        }
+    }
+}
---
+++ src/Midiot/MidiHandler.cs
@@ -0,0 +1,169 @@
+namespace Midiot
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using NAudio.Midi;
+
+    public class MidiHandler : IDisposable
+    {
+        private readonly List<string> commands;
+        private int current;
+
+        private readonly MidiIn midiIn;
+        private readonly MidiOut midiOut;
+
+        private int currentChannel;
+
+        public MidiHandler(int inIndex, int outIndex)
+        {
+            this.commands = new List<string>();
+            this.current = 0;
+            this.currentChannel = 0;
+
+            this.midiIn = new MidiIn(inIndex);
+            this.midiOut = new MidiOut(outIndex);
+        }
+
+        public bool SendInitialMessage { get; set; }
+
+        public event EventHandler Input;
+
+        public void Reset()
+        {
+            this.current = 0;
+        }
+
+        public async Task LoadAsync(string filename)
+        {
+            var data = await File.ReadAllLinesAsync(filename);
+            this.commands.AddRange(data.Where(x => !x.StartsWith('#')));
+        }
+
+        public void Start()
+        {
+            this.midiIn.MessageReceived += this.MidiInOnMessageReceived;
+            this.midiIn.Start();
+
+            if (this.SendInitialMessage)
+            {
+                this.Next();
+            }
+        }
+
+        private void MidiInOnMessageReceived(object sender, MidiInMessageEventArgs e)
+        {
+            if (e.MidiEvent.CommandCode == MidiCommandCode.PatchChange)
+            {
+                this.Next();
+            }
+        }
+
+        public void Stop()
+        {
+            this.midiIn.Stop();
+        }
+
+        public static IEnumerable<string> GetOutputDevices()
+        {
+            for (var device = 0; device < MidiOut.NumberOfDevices; device++)
+            {
+                yield return MidiOut.DeviceInfo(device).ProductName;
+            }
+        }
+
+        public static IEnumerable<string> GetInputDevices()
+        {
+            for (var device = 0; device < MidiIn.NumberOfDevices; device++)
+            {
+                yield return MidiIn.DeviceInfo(device).ProductName;
+            }
+        }
+
+        public string GetCurrentCommand()
+        {
+            return this.commands[this.current];
+        }
+
+        public int GetCurrentIndex()
+        {
+            return this.current;
+        }
+
+        public IEnumerable<string> GetCommandList()
+        {
+            return this.commands;
+        }
+
+        private void Next()
+        {
+            this.OnInput();
+
+            if (!this.commands.Any())
+            {
+                return;
+            }
+
+            if (this.current >= this.commands.Count)
+            {
+                return;
+            }
+
+            // format: [Label] <commands[]>
+            // commands: CH1 PC1 CC34,0
+            var message = this.commands[this.current];
+            var midiEvents = new List<MidiEvent>();
+
+            var tokens = (message.StartsWith('[') ? message.Substring(message.LastIndexOf(']') + 1) : message).Split(' ');
+            foreach (var token in tokens.Where(x => !string.IsNullOrWhiteSpace(x)))
+            {
+                var midiCommand = token.Substring(0, 2);
+                var value = token.Substring(2);
+
+                switch (midiCommand)
+                {
+                    case "CH":
+                        this.currentChannel = int.Parse(value);
+                        break;
+
+                    case "PC":
+                        var programChange = new PatchChangeEvent(0, this.currentChannel, int.Parse(value));
+                        midiEvents.Add(programChange);
+
+                        break;
+
+                    case "CC":
+                        var data = value.Split(',');
+                        var controlChange = new ControlChangeEvent(0, this.currentChannel, (MidiController)int.Parse(data[0]), int.Parse(data[1]));
+                        midiEvents.Add(controlChange);
+
+                        break;
+
+                    default:
+                        // not supported
+                        break;
+                }
+            }
+
+            foreach (var midiEvent in midiEvents)
+            {
+                this.midiOut.Send(midiEvent.GetAsShortMessage());
+            }
+
+            this.current = Math.Min(this.commands.Count - 1, this.current + 1);
+        }
+
+        public void Dispose()
+        {
+            this.midiIn?.Dispose();
+            this.midiOut?.Dispose();
+        }
+
+        protected virtual void OnInput()
+        {
+            this.Input?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
